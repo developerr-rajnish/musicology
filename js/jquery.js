fea5fd9adc1a734adfd99521e42c224b86c967d9
RAJNISH648 <79629938+RAJNISH648@users.noreply.github.com>
2021-10-10 23:26:07 +0530

    Final Website

---
+++ js/jquery.js
@@ -0,0 +1,13 @@
+$(".hamburger").click(function() {
+    $(".navigation").toggleClass("active");
+    $(".line-1").toggleClass("rotate-line-1");
+    $(".line-3").toggleClass("rotate-line-2");
+    $(".line-2").toggleClass("remove-line-3");
+});
+
+
+
+
+
+
+   
